2384819e854d43b167281cf808daca8a3bba3374
duexcoast <conor.ux@gmail.com>
2023-01-04 12:55:01 -0500

    integration test for InMemoryPlayerStore - passing

---
+++ in_memory_player_store.go
@@ -0,0 +1,17 @@
+package main
+
+func NewInMemoryPlayerStore() *InMemPlayerStore {
+	return &InMemPlayerStore{map[string]int{}}
+}
+
+type InMemPlayerStore struct {
+	store map[string]int
+}
+
+func (i *InMemPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func (i *InMemPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
