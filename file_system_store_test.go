f9db185221534cc60596391f6c26837c96b0f169
duexcoast <conor.ux@gmail.com>
2023-01-09 16:51:47 -0500

    implemented tempFile test

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
-	database := strings.NewReader(`[
+	t.Run("league from a reader", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
 
-	t.Run("league from a reader", func(t *testing.T) {
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -29,21 +31,44 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Cleo")
-
 		want := 10
 
 		assertScoreEquals(t, got, want)
 	})
 }
 
-func assertScoreEquals(t *testing.T, got, want int) {
+func assertScoreEquals(t testing.TB, got, want int) {
 	t.Helper()
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
+}
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpfile, err := os.CreateTemp("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file, %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
