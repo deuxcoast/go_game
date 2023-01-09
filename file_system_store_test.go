dfc27170a48adf57bbf539b75d5867927d4b715e
duexcoast <conor.ux@gmail.com>
2023-01-09 15:50:43 -0500

    GetPlayerScore implemented on FileSystemPlayerStore

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestFileSystemStore(t *testing.T) {
-
-	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+	database := strings.NewReader(`[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
+
+	t.Run("league from a reader", func(t *testing.T) {
 
 		store := FileSystemPlayerStore{database}
 
@@ -27,4 +27,23 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Cleo")
+
+		want := 10
+
+		assertScoreEquals(t, got, want)
+	})
+}
+
+func assertScoreEquals(t *testing.T, got, want int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
--- league.go
+++ league.go
@@ -8,9 +8,12 @@ import (
 
 func NewLeague(rdr io.Reader) ([]Player, error) {
 	var league []Player
+
 	err := json.NewDecoder(rdr).Decode(&league)
+
 	if err != nil {
 		err = fmt.Errorf("problem parsing league, %v", err)
 	}
+
 	return league, err
 }
