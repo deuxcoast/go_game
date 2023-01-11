4162495431ba3ad87c5b419b526452b450bad381
duexcoast <conor.ux@gmail.com>
2023-01-10 23:33:33 -0500

    test written but failing for correct scheduled alerts based on players entered

--- cli_test.go
+++ cli_test.go
@@ -75,7 +75,10 @@ func TestCLI(t *testing.T) {
 
 	t.Run("it prompts the user to enter the number of players", func(t *testing.T) {
 		stdout := &bytes.Buffer{}
-		cli := poker.NewCLI(dummyPlayerStore, dummyStdIn, stdout, dummyBlindAlerter)
+		in := strings.NewReader("7\n")
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := poker.NewCLI(dummyPlayerStore, in, stdout, blindAlerter)
 		cli.PlayPoker()
 
 		got := stdout.String()
@@ -83,6 +86,24 @@ func TestCLI(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
+		}
+
+		cases := []scheduledAlert{
+			{0 * time.Second, 100},
+			{12 * time.Minute, 200},
+			{24 * time.Minute, 300},
+			{36 * time.Minute, 400},
+		}
+
+		for i, want := range cases {
+			t.Run(fmt.Sprint(want), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d was not scheduled, %v", i, blindAlerter.alerts)
+				}
+
+				got := blindAlerter.alerts[i]
+				assertScheduledAlert(t, got, want)
+			})
 		}
 	})
 }
@@ -105,6 +126,8 @@ func (s *SpyBlindAlerter) ScheduleAlertAt(at time.Duration, amount int) {
 }
 
 func assertScheduledAlert(t testing.TB, got, want scheduledAlert) {
+	t.Helper()
+
 	if got.amount != want.amount {
 		t.Errorf("got amount %d want %d", got.amount, want.amount)
 	}
