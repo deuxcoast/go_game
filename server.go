24c205847da97a5ca7d3b48bc16bf3969feb3158
duexcoast <conor.ux@gmail.com>
2023-01-11 16:38:20 -0500

    :5000/game succesfully rendering template

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package poker
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strings"
 )
@@ -31,7 +32,7 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 	router := http.NewServeMux()
 	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
 	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
-	router.Handle("/game", http.HandlerFunc(p.gameHandler))
+	router.Handle("/game", http.HandlerFunc(p.game))
 
 	p.Handler = router
 	return p
@@ -56,8 +57,13 @@ func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (p *PlayerServer) gameHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+func (p *PlayerServer) game(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("game.html")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("problem loading template %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
 }
 
 func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
