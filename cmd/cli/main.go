e964ef4bae2db2e45cfdce208ba4682a37c1a316
duexcoast <conor.ux@gmail.com>
2023-01-10 19:31:00 -0500

    abstracted FileSystemPlayerStoreFromFile and used the func in both cli and webserver

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -11,21 +11,16 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer close()
+
 	fmt.Println("lets play poker")
 	fmt.Println("Type: '{name} wins' to record a win")
 
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-
-	if err != nil {
-		log.Fatalf("problem opening file %s, %v", dbFileName, err)
-	}
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
-
-	game := poker.CLI{store, os.Stdin}
-	game.PlayPoker()
+	poker.NewCLI(store, os.Stdin).PlayPoker()
 }
--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	poker "github.com/duexcoast/go_game"
 )
@@ -11,18 +10,15 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-	if err != nil {
-		log.Fatalf("problem creating the file system player store, %v", err)
-	}
+	defer close()
 
 	server := poker.NewPlayerServer(store)
+
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
