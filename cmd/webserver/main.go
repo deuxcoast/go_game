92ae6060d8b709411073c20f659d5e293d5f45fc
duexcoast <conor.ux@gmail.com>
2023-01-12 12:21:18 -0500

    webserver/main is using Game in PlayerServer

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	poker "github.com/duexcoast/go_game"
 )
@@ -10,16 +11,23 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem opening %s, %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("problem creating file system player store, %v", err)
 	}
-	defer close()
 
-	server := poker.NewPlayerServer(store)
+	game := poker.NewTexasHoldEm(poker.BlindAlerterFunc(poker.Alerter), store)
+	server, err := poker.NewPlayerServer(store, game)
 
-	if err := http.ListenAndServe(":5000", server); err != nil {
-		log.Fatalf("could not listen on port 5000 %v", err)
+	if err != nil {
+		log.Fatalf("problem creating player server, %v", err)
 	}
+
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
